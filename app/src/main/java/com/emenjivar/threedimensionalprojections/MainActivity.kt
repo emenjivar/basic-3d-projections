cfe68866d90ae15a21a8dbd0b14321d19f352599
emenjivar <carlos.itca@gmail.com>
2025-11-08 09:42:52 -0600

    refactor: Move matrices logic to its own file

--- app/src/main/java/com/emenjivar/threedimensionalprojections/MainActivity.kt
+++ app/src/main/java/com/emenjivar/threedimensionalprojections/MainActivity.kt
@@ -5,23 +5,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.animateFloat
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableFloatStateOf
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -29,12 +17,12 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.tooling.preview.Preview
-import com.emenjivar.threedimensionalprojections.math.MatricesOperator
-import com.emenjivar.threedimensionalprojections.math.MatricesOperator.multiply
+import com.emenjivar.threedimensionalprojections.math.multiply
+import com.emenjivar.threedimensionalprojections.math.projectionMatrix
+import com.emenjivar.threedimensionalprojections.math.xRotationMatrix
+import com.emenjivar.threedimensionalprojections.math.yRotationMatrix
+import com.emenjivar.threedimensionalprojections.math.zRotationMatrix
 import com.emenjivar.threedimensionalprojections.ui.theme.ThreeDimensionalProjectionsTheme
-import kotlin.math.cos
-import kotlin.math.sin
 
 private const val ANIMATION_TIME = 5000
 
@@ -85,7 +73,8 @@ class MainActivity : ComponentActivity() {
                 var _yRotation by remember { mutableFloatStateOf(0f) }
 
                 Canvas(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .pointerInput(Unit) {
                             detectDragGestures { change, dragAmount ->
                                 change.consume()
@@ -94,7 +83,10 @@ class MainActivity : ComponentActivity() {
                                 _yRotation += -(dragAmount.x * slowFactor % 360f)
                                 _xRotation += dragAmount.y * slowFactor % 360f
 
-                                Log.wtf("MainActivity", "drag (x: ${dragAmount.x}, y: ${dragAmount.y}), rotation: ($_yRotation,$_xRotation) ")
+                                Log.wtf(
+                                    "MainActivity",
+                                    "drag (x: ${dragAmount.x}, y: ${dragAmount.y}), rotation: ($_yRotation,$_xRotation) "
+                                )
                             }
                         }
                 ) {
@@ -153,45 +145,6 @@ class MainActivity : ComponentActivity() {
 data class Coordinate(val x: Float, val y: Float)
 data class Coordinate3D(val x: Float, val y: Float, val z: Float)
 
-private val projectionMatrix = arrayOf(
-    floatArrayOf(1f, 0f, 0f),
-    floatArrayOf(0f, 1f, 0f),
-    floatArrayOf(0f, 0f, 0f),
-)
-
-/**
- * @param angle value in radians
- */
-private fun xRotationMatrix(angle: Float): Array<FloatArray> {
-    return arrayOf(
-        floatArrayOf(1f, 0f, 0f),
-        floatArrayOf(0f, cos(angle), -sin(angle)),
-        floatArrayOf(0f, sin(angle), cos(angle)),
-    )
-}
-
-/**
- * @param angle value in radians
- */
-private fun yRotationMatrix(angle: Float): Array<FloatArray> {
-    return arrayOf(
-        floatArrayOf(cos(angle), 0f, sin(angle)),
-        floatArrayOf(0f, 1f, 0f),
-        floatArrayOf(-sin(angle), 0f, cos(angle)),
-    )
-}
-
-/**
- * @param angle value in radians
- */
-private fun zRotationMatrix(angle: Float): Array<FloatArray> {
-    return arrayOf(
-        floatArrayOf(cos(angle), -sin(angle), 0f),
-        floatArrayOf(sin(angle), cos(angle), 0f),
-        floatArrayOf(0f, 0f, 1f),
-    )
-}
-
 /**
  * @param alpha Angle in degrees.
  * @param beta Angle in degrees.
@@ -220,12 +173,16 @@ fun project3DCoordinate(
     val yRotated = yRotationMatrix(betaRad) multiply xRotated
     val zRotated = zRotationMatrix(gammaRad) multiply yRotated
 
+    // Apply perspective projection
+    val distance = 2.5f // Distance from camera
+    val w = 1f / (distance - zRotated[2][0]) // perpective divide factor
+
     val projected = projectionMatrix multiply zRotated
 
     // Assuming zRotated as 1x2 matrix
     return Coordinate(
-        x = projected[0][0],
-        y = projected[1][0]
+        x = projected[0][0] * w,
+        y = projected[1][0] * w
     )
 //    return Coordinate(
 //        x = x * cos(betaRad) * cos(gammaRad) - y * cos(betaRad) * sin(gammaRad) + z * sin(betaRad),
@@ -242,5 +199,5 @@ fun DrawScope.normalizeWidth(coordinate: Float): Float {
 }
 
 fun DrawScope.normalizeHeight(coordinate: Float): Float {
-    return size.width  / 2f * coordinate + size.height / 2f
+    return size.width / 2f * coordinate + size.height / 2f
 }
--- app/src/main/java/com/emenjivar/threedimensionalprojections/math/matrices.kt
+++ app/src/main/java/com/emenjivar/threedimensionalprojections/math/matrices.kt
@@ -1,40 +1,76 @@
 package com.emenjivar.threedimensionalprojections.math
 
-object MatricesOperator {
-    /**
-     * Assuming [a] and [b] as vectors with the same length
-     */
-    infix fun FloatArray.dotProduct(b: FloatArray): Float {
-        var result = 0f
-        for (i in 0..this.size - 1) {
-            result += this[i] * b[i]
-        }
-        return result
+import kotlin.math.cos
+import kotlin.math.sin
+
+val projectionMatrix = arrayOf(
+    floatArrayOf(1f, 0f, 0f),
+    floatArrayOf(0f, 1f, 0f),
+    floatArrayOf(0f, 0f, 0f),
+)
+
+fun xRotationMatrix(
+    angle: Float
+): Array<FloatArray> {
+    return arrayOf(
+        floatArrayOf(1f, 0f, 0f),
+        floatArrayOf(0f, cos(angle), -sin(angle)),
+        floatArrayOf(0f, sin(angle), cos(angle)),
+    )
+}
+
+fun yRotationMatrix(
+    angle: Float
+): Array<FloatArray> {
+    return arrayOf(
+        floatArrayOf(cos(angle), 0f, sin(angle)),
+        floatArrayOf(0f, 1f, 0f),
+        floatArrayOf(-sin(angle), 0f, cos(angle)),
+    )
+}
+
+fun zRotationMatrix(
+    angle: Float
+): Array<FloatArray> {
+    return arrayOf(
+        floatArrayOf(cos(angle), -sin(angle), 0f),
+        floatArrayOf(sin(angle), cos(angle), 0f),
+        floatArrayOf(0f, 0f, 1f),
+    )
+}
+
+infix fun FloatArray.dotProduct(b: FloatArray): Float {
+    var result = 0f
+    for (i in 0..this.size - 1) {
+        result += this[i] * b[i]
+    }
+    return result
+}
+
+infix fun Array<FloatArray>.multiply(
+    b: Array<FloatArray>
+): Array<FloatArray> {
+    val rowsA = this.size
+    val columnsA = this[0].size
+    val rowsB = b.size
+    val columnsB = b[0].size
+
+    if (columnsA != rowsB) {
+        throw IllegalArgumentException(
+            "Matrix dimension incompatible: ($rowsA x $columnsB). Columns of A must equals rows of B."
+        )
     }
 
-    infix fun Array<FloatArray>.multiply(
-        b: Array<FloatArray>
-    ): Array<FloatArray> {
-        val rowsA = this.size
-        val columnsA = this[0].size
-        val rowsB = b.size
-        val columnsB = b[0].size
+    // Result matrix will be rowsA * columnsB
+    val result = Array(rowsA) { FloatArray(columnsB) }
 
-        if (columnsA != rowsB) {
-            throw Exception("The rows of a must equal the number of columns in b")
+    // Fill the output matrix
+    for (rowA in 0..rowsA - 1) {
+        for (colB in 0..columnsB - 1) {
+            val columnB = FloatArray(rowsB) { rowB -> b[rowB][colB] }
+            result[rowA][colB] = this[rowA] dotProduct columnB
         }
-
-        // Result matrix will be rowsA * columnsB
-        val result = Array(rowsA) { FloatArray(columnsB) }
-
-        // Fill the output matrix
-        for (rowA in 0..rowsA - 1) {
-            for (colB in 0..columnsB - 1) {
-                val columnB = FloatArray(rowsB) { rowB -> b[rowB][colB] }
-                result[rowA][colB] = this[rowA] dotProduct columnB
-            }
-        }
-
-        return result
     }
+
+    return result
 }
